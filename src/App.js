021d3015cf46c10e6dd9a05da75eaf2b5ca92a8a
feyzi-erdal <95186565+feyzi-erdal@users.noreply.github.com>
2022-07-10 20:33:08 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Navbar from './components/navbar/Navbar';
+import './App.scss';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
